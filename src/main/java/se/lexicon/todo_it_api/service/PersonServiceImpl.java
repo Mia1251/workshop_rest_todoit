ea98b93c260c6745f100cd0ccb0591caf205f120
Mia1251 <svanteomax@gmail.com>
2021-10-19 10:27:27 +0200

    Commiting for security

--- src/main/java/se/lexicon/todo_it_api/service/PersonServiceImpl.java
+++ src/main/java/se/lexicon/todo_it_api/service/PersonServiceImpl.java
@@ -1,11 +1,28 @@
 package se.lexicon.todo_it_api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import se.lexicon.todo_it_api.conversions.ConversionService;
+import se.lexicon.todo_it_api.data.PersonDAO;
+import se.lexicon.todo_it_api.data.TodoItemDAO;
 import se.lexicon.todo_it_api.dto.PersonDto;
 import se.lexicon.todo_it_api.form.PersonFormDto;
+import se.lexicon.todo_it_api.model.entity.Person;
 
 import java.util.List;
+import java.util.Optional;
 
 public class PersonServiceImpl implements PersonService{
+
+    private final PersonDAO personDao;
+    private final TodoItemDAO todoItemDao;
+    private final ConversionService convert;
+
+    @Autowired
+    public PersonServiceImpl(PersonDAO personDao, TodoItemDAO todoItemDao, ConversionService convert) {
+        this.personDao = personDao;
+        this.todoItemDao = todoItemDao;
+        this.convert = convert;
+    }
 
     @Override
     public PersonDto addTodoItem(Integer personId, Integer todoItemId) {
@@ -15,17 +32,22 @@ public class PersonServiceImpl implements PersonService{
     @Override
     public PersonDto create(PersonFormDto personFormDto) {
 
+        Person saved = personDao.save(convert.toPerson(personFormDto));
 
-        return null;
+        return convert.toPersonDto(saved);
     }
 
     @Override
     public boolean delete(Integer personId) {
-        return false;
+
+        personDao.deleteById(personId);
+
+        return personDao.findById(personId).isPresent();
     }
 
     @Override
     public List<PersonDto> findAll() {
+
         return null;
     }
 
@@ -46,6 +68,7 @@ public class PersonServiceImpl implements PersonService{
 
     @Override
     public PersonDto update(Integer personId, PersonFormDto personFormDto) {
+
         return null;
     }
 }
